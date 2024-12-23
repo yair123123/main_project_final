ca5f3beb9581c4395b874b472f0df2949abc77a9
DELL <ytykhn8@gmail.com>
2024-12-23 18:46:13 +0200

    add endpoints for quries relation

--- app/controllers/relation_blueprint.py
+++ app/controllers/relation_blueprint.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, jsonify
-from app.dbs.mongodb.repository import most_deadly, average_casualties_by_area, top_5_most_num_spread, \
-    most_group_active_by_region, history_events_by_year
-from app.dbs.neo4j.repository.relation_repository import groups_with_common_goal_by_region
+
+from app.dbs.neo4j.repository.relation_repository import groups_with_common_goal_by_region, \
+    groups_with_common_goal_by_country, both_groups_in_event_by_country, both_groups_in_event_by_region, \
+    shared_goals_in_groups_by_year, shared_attack_type_in_groups_by_region, shared_attack_type_in_groups_by_country
 
 relation_blueprint = Blueprint('relation', __name__)
 
@@ -11,53 +12,63 @@ def index():
         "message": "Please enter stat for result!"
     })
 
-@relation_blueprint.route('/most_deadly/<string:country>')
-def get_groups_with_common_goal_by_region(country:str):
+@relation_blueprint.route('/groups_with_common_goal_by_location/<string:country>')
+def get_groups_with_common_goal_by_location(country:str):
     try:
-        res = groups_with_common_goal_by_region(bool(country))
+        if not bool(country):
+            res = groups_with_common_goal_by_region()
+        else:
+            res = groups_with_common_goal_by_country()
+        return jsonify({
+            "res": res
+        }),200
+
+    except Exception as e:
+        return jsonify({"message":str(e)}),500
+
+@relation_blueprint.route('/both_groups_in_event_by_location/<string:country>')
+def get_both_groups_in_event_by_location(country:str):
+    try:
+        if not bool(country):
+            res = both_groups_in_event_by_region()
+        else:
+            res = both_groups_in_event_by_country()
         return jsonify({
             "res": res
         }),200
     except Exception as e:
         return jsonify({"message":str(e)}),500
 
-@relation_blueprint.route('/average_casualties_by_area/<string:top>')
-def average_casualties(top:str):
+@relation_blueprint.route('/shared_goals_in_groups_by_year/')
+def get_shared_goals_in_groups_by_year():
     try:
-        res = average_casualties_by_area(bool(top))
+        res = shared_goals_in_groups_by_year()
         return jsonify({
             "res": res
         }),200
     except Exception as e:
         return jsonify({"message":str(e)}),500
 
-@relation_blueprint.route('/top_5_most_num_spread/')
-def get_top_5_most_num_spread():
+@relation_blueprint.route('/shared_attack_type_in_groups_by_location/')
+def get_shared_attack_type_in_groups_by_location(country:str):
     try:
-        res = top_5_most_num_spread()
+        if not bool(country):
+            res = shared_attack_type_in_groups_by_region()
+        else:
+            res = shared_attack_type_in_groups_by_country()
         return jsonify({
             "res": res
         }),200
     except Exception as e:
         return jsonify({"message":str(e)}),500
 
-@relation_blueprint.route('/most_group_active_by_region/')
-def get_most_group_active_by_region():
-    try:
-        res = most_group_active_by_region()
-        return jsonify({
-            "res": res
-        }),200
-    except Exception as e:
-        return jsonify({"message":str(e)}),500
-
-@relation_blueprint.route('/history_events_by_year/<int:year>')
-def get_history_events_by_year(year:int):
-    try:
-        res = history_events_by_year(year)
-        return jsonify({
-            "res": res
-        }),200
-    except Exception as e:
-        return jsonify({"message":str(e)}),500
+# @relation_blueprint.route('/history_events_by_year/<int:year>')
+# def get_history_events_by_year(year:int):
+#     try:
+#         res = history_events_by_year(year)
+#         return jsonify({
+#             "res": res
+#         }),200
+#     except Exception as e:
+#         return jsonify({"message":str(e)}),500
 
--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
+from app.controllers.relation_blueprint import relation_blueprint
 from app.controllers.stat_blueprint import stat_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(stat_blueprint, url_prefix='/api/stat')
+app.register_blueprint(relation_blueprint,url_prefix="/api/relation")
 if __name__ == '__main__':
     app.run()
